95f8485839e9a1cfd9d8051fe2b9085cabb10aaa
stefanilima <stefanisilvadelima@hotmail.com>
2022-05-25 20:24:12 -0300

    Exercicios 6, 7 e 8

---
+++ 30-days-of-code/day_06:Lets_Review.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Objective
+
+Today we will expand our knowledge of strings, combining it with what we have already learned about loops.
+Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given a string, S , of length N that is indexed from 0 to N - 1, print its even-indexed and odd-indexed
+characters as  space-separated strings on a single line (see the Sample below for more detail).
+
+Note:  is considered to be an even index.
+
+Example
+a = adbecf
+
+Print abc def
+
+Input Format
+
+The first line contains an integer, T (the number of test cases).
+Each line  of the  subsequent lines contain a string, .
+
+Output Format
+
+For each String  (where ), print 's even-indexed characters, followed by a space,
+followed by 's odd-indexed characters.
+
+Sample Input
+
+2
+Hacker
+Rank
+
+Sample Output
+
+Hce akr
+Rn ak
+
+The even indices are , , and , and the odd indices are , , and .
+We then print a single line of  space-separated strings;
+the first string contains the ordered characters from 's even indices (),
+and the second string contains the ordered characters from 's odd indices ().
+
+The even indices are 0 and 2, and the odd indices are 1 and 3.
+We then print a single line of 2 space-separated strings;
+the first string contains the ordered characters from 's even indices (),
+and the second string contains the ordered characters from 's odd indices ().
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int T = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            string S = Console.ReadLine();
+            string letrasPares = null;
+            string letrasImpares = null;
+
+            for (int j = 0; j < S.Length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    letrasPares += S[j];
+                }
+                else
+                {
+                    letrasImpares += S[j];
+                }
+            }
+
+            Console.WriteLine(letrasPares + " " + letrasImpares);
+        }
+    }
+}
---
+++ 30-days-of-code/day_7:Arrays.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * 
+ */
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        string saida = null;
+        for (int i = arr.Count - 1; i >= 0; i--)
+        {
+            saida += arr[i] + " ";
+        }
+
+        Console.WriteLine(saida);
+    }
+}
